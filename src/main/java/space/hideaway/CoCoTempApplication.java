2d4f50475facc25b424d31560622c623eb662dae
Piper Dougherty <doughertypiper@gmail.com>
2016-09-21 23:14:49 -0400

    Some files didn't get sent up.

---
+++ src/main/java/space/hideaway/CoCoTempApplication.java
@@ -0,0 +1,12 @@
+package space.hideaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCoTempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoCoTempApplication.class, args);
+    }
+}
