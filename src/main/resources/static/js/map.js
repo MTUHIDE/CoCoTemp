6483ebdb1defd99c752076de649de569e2fc0d75
Caden Sumner <cadenjsumner@gmail.com>
2016-10-07 14:50:17 -0400

    Map now works, some style issues.
    Need to begin ensuring mobile optimizing works -- bottom absolute
    positioned row might cause trouble.
    
    Ideally on screen resize the left bar would transition to a top bar and
    the bottom row would expand to the full width.
    
    Mobile would have a circular menu icon in the corner.
    
    Also attempting to put a favicon, not sure where spring wants it so it's
    in src currently.

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -2,21 +2,20 @@
  * Created by caden on 10/7/2016.
  */
 
-
-
+var maxBounds = L.latLngBounds(
+    L.latLng(5.499550, -167.276413), //Southwest
+    L.latLng(83.162102, -52.233040)  //Northeast
+);
 /* Initialize Map */
-var map = L.map('bgmap');
+var map = L.map('bgmap', {
+    dragging: true,
+    zoomControl: false
+});
+map.setView([40, -85], 6);
+map.setMaxBounds(maxBounds);
 
 /* Style layer from MapBox */
 L.tileLayer('https://api.mapbox.com/styles/v1/cjsumner/citzzhm4m002f2in5jfc102im/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2pzdW1uZXIiLCJhIjoiY2lmeDhkMDB3M3NpcHUxbTBlZnoycXdyYyJ9.NKtr-pvthf3saPDsRDGTmw',
     {
-        maxZoom: 17,
-        minZoom: 9
+        maxZoom: 18
     }).addTo(map);
-
-L.map('bgmap').setView([51.505, -30.9], 13);
-L.map('bgmap').dragging(true);
-
-
-
-
