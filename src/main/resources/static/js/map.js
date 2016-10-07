9d97a25eea586df6620bb845fedf5a0712516b7d
Caden Sumner <cadenjsumner@gmail.com>
2016-10-07 12:45:02 -0400

    Map + login changes
    Map was working, removed some code to break it again (feature)
    
    Color + styling

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -2,9 +2,21 @@
  * Created by caden on 10/7/2016.
  */
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/cjsumner/citzeefn400012hqh2y2eo2gv/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2pzdW1uZXIiLCJhIjoiY2lmeDhkMDB3M3NpcHUxbTBlZnoycXdyYyJ9.NKtr-pvthf3saPDsRDGTmw}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: 'pk.eyJ1IjoiY2pzdW1uZXIiLCJhIjoiY2lmeDhkMDB3M3NpcHUxbTBlZnoycXdyYyJ9.NKtr-pvthf3saPDsRDGTmw}'
-}).addTo(mymap);
+
+
+/* Initialize Map */
+var map = L.map('bgmap');
+
+/* Style layer from MapBox */
+L.tileLayer('https://api.mapbox.com/styles/v1/cjsumner/citzzhm4m002f2in5jfc102im/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2pzdW1uZXIiLCJhIjoiY2lmeDhkMDB3M3NpcHUxbTBlZnoycXdyYyJ9.NKtr-pvthf3saPDsRDGTmw',
+    {
+        maxZoom: 17,
+        minZoom: 9
+    }).addTo(map);
+
+L.map('bgmap').setView([51.505, -30.9], 13);
+L.map('bgmap').dragging(true);
+
+
+
+
