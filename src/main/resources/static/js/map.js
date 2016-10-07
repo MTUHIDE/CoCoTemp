ff1d2e80a74dbf61347af9bcb2cb1e0cc8f9e676
Caden Sumner <cadenjsumner@gmail.com>
2016-10-07 03:11:18 -0400

    Leaflet integration
    Map doesn't seem to be loading correctly. Layout still being changes.

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,11 @@
+/**
+ * Created by caden on 10/7/2016.
+ */
+
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/cjsumner/citzeefn400012hqh2y2eo2gv/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2pzdW1uZXIiLCJhIjoiY2lmeDhkMDB3M3NpcHUxbTBlZnoycXdyYyJ9.NKtr-pvthf3saPDsRDGTmw}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'citzeefn400012hqh2y2eo2gv',
+    accessToken: 'pk.eyJ1IjoiY2pzdW1uZXIiLCJhIjoiY2lmeDhkMDB3M3NpcHUxbTBlZnoycXdyYyJ9.NKtr-pvthf3saPDsRDGTmw}'
+}).addTo(mymap);
