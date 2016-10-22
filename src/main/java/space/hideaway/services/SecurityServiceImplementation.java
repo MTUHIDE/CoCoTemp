5ec2a160960c21b8914b83ed0a9b469a4fd2c7d6
Piper Dougherty <doughertypiper@gmail.com>
2016-10-22 06:18:31 -0400

    VERY IMPORTANT FIX --The login controller was logically flawed. If a user typed in their username without a password, they would correctly log in. I have fixed this. The reason was a bad credential exception not being handled.
    Signed-off-by: Piper Dougherty <doughertypiper@gmail.com>

--- src/main/java/space/hideaway/services/SecurityServiceImplementation.java
+++ src/main/java/space/hideaway/services/SecurityServiceImplementation.java
@@ -3,11 +3,9 @@ package space.hideaway.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -54,30 +52,25 @@ public class SecurityServiceImplementation implements SecurityService {
     }
 
     public String tryLogin(String username, String password) {
-        UserDetails userDetails = null;
-
         try {
-            userDetails = userDetailsServiceImplementation.loadUserByUsername(username);
-        } catch (UsernameNotFoundException e) {
-            //TODO use GSON library.
+            UserDetails userDetails = userDetailsServiceImplementation.loadUserByUsername(username);
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails,
+                    password,
+                    userDetails.getAuthorities()
+            );
+            authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+            if (usernamePasswordAuthenticationToken.isAuthenticated()) {
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                return "{\"status\": true, " +
+                        "\"location\": \"/dashboard\"}";
+            } else {
+                SecurityContextHolder.getContext().setAuthentication(null);
+            }
+        } catch (AuthenticationException e) {
             return "{\"status\": false, \"error\": \"Username or password is incorrect.\"}";
         }
 
-        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                userDetails,
-                password,
-                userDetails.getAuthorities()
-        );
-        try {
-            authenticationManager.authenticate(usernamePasswordAuthenticationToken);
-        } catch (AuthenticationException e) {
-            e.printStackTrace();
-        }
-        if (usernamePasswordAuthenticationToken.isAuthenticated()) {
-            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-            return "{\"status\": true, " +
-                    "\"location\": \"/dashboard\"}";
-        }
         return "{\"status\": false, \"error\": \"Username or password is incorrect.\"}";
     }
 }
