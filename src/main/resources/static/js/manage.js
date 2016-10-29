68643e4dbaa58f3da729b9b98c2128828063271f
doughepi <doughertypiper@gmail.com>
2016-10-28 23:07:23 -0700

    Started the device management pages with Google autocomplete.

--- src/main/resources/static/js/manage.js
+++ src/main/resources/static/js/manage.js
@@ -1,3 +1,16 @@
 /**
  * Created by dough on 10/28/2016.
  */
+jQuery(document).ready(function () {
+
+    //Initialize location search box.
+    var input = document.getElementById('device-location');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    google.maps.event.addDomListener(autocomplete, 'load', function () {
+
+    });
+
+    $('#new-device').click(function () {
+
+    })
+});
