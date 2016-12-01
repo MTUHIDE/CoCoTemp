391d9885853c696af7f0b10f9e59c7211a69dc3c
Paul Esch-Laurent <herppfel@gmail.com>
2016-11-30 21:07:23 -0500

    Add Gulp

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp    = require("gulp"),
+    plugins = require("gulp-load-plugins")(),
+    pkg     = require("./package.json"),
+    paths   = {
+        src: {
+            css: "./src/main/resources/static/sass"
+        },
+        dist: {
+            css: "./src/main/resources/static/css"
+        }
+    },
+    config  = {
+        autoprefixer: {
+            browsers: ["last 2 versions", "> 1%"]
+        }
+    };
+
+// Build the Sass
+gulp.task("build:css", function() {
+    return gulp.src(paths.src.css + "/**/*")
+        // Prevents Sass from hanging on errors (syntax error, etc.)
+        .pipe(plugins.plumber())
+        .pipe(plugins.sourcemaps.init())
+        // Need to use sass.sync() to work with Plumber correctly
+        .pipe(plugins.sass.sync())
+        .pipe(plugins.autoprefixer(config.autoprefixer))
+        .pipe(gulp.dest(paths.dist.css));
+});
+
+// Clean the Sass build directory
+gulp.task("clean:css", function() {
+    return gulp.src(paths.dist.css, { read: false })
+        .pipe(plugins.clean());
+})
+
+gulp.task("build", ["build:css"]);
+
+gulp.task("clean", ["clean:css"]);
+
+gulp.task("watch", function() {
+    gulp.src(paths.src.css+ "/**/*", ["build:css"]);
+});
