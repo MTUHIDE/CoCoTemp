3bb3878c0a3e16797fca91bfd07bbc474d8864dd
doughepi <doughertypiper@gmail.com>
2017-01-19 20:33:00 -0500

    Rewrote the registration page to better fit theme of wesite.

--- src/main/java/space/hideaway/controllers/RegisterController.java
+++ src/main/java/space/hideaway/controllers/RegisterController.java
@@ -2,13 +2,7 @@ package space.hideaway.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import space.hideaway.UserValidator;
-import space.hideaway.model.User;
 import space.hideaway.services.SecurityService;
 import space.hideaway.services.UserService;
 
@@ -37,15 +31,15 @@ public class RegisterController {
      * @param model The model maintained by Spring.
      * @return The name of the registration page template.
      */
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public String loadRegistration(Model model) {
-
-        //Inject a new user into the form to be filled out and returned.
-        model.addAttribute("user", new User());
-
-        //Refers to the register.html file.
-        return "register";
-    }
+//    @RequestMapping(value = "/register", method = RequestMethod.GET)
+//    public String loadRegistration(Model model) {
+//
+//        //Inject a new user into the form to be filled out and returned.
+//        model.addAttribute("user", new User());
+//
+//        //Refers to the register.html file.
+//        return "register";
+//    }
 
     /**
      * The endpoint for registration form submitting.
@@ -56,24 +50,24 @@ public class RegisterController {
      * @return The name of the template to be returned. Reloads the registration page if errors where found,
      * otherwise, redirects to the dashboard.
      */
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
-        //Grab the data entered by the user.
-        String username = userForm.getUsername();
-        String password = userForm.getPassword();
-
-        //Validate the form.
-        userValidator.validate(userForm, bindingResult);
-
-        if (bindingResult.hasErrors()) {
-
-            return "register";
-        }
-
-        //At this point, all the validation has passed. Save the new account and login the user.
-        userService.save(userForm);
-        securityService.autoLogin(username, password);
-
-        return "redirect:/dashboard";
-    }
+//    @RequestMapping(value = "/register", method = RequestMethod.POST)
+//    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+//        //Grab the data entered by the user.
+//        String username = userForm.getUsername();
+//        String password = userForm.getPassword();
+//
+//        //Validate the form.
+//        userValidator.validate(userForm, bindingResult);
+//
+//        if (bindingResult.hasErrors()) {
+//
+//            return "register";
+//        }
+//
+//        //At this point, all the validation has passed. Save the new account and login the user.
+//        userService.save(userForm);
+//        securityService.autoLogin(username, password);
+//
+//        return "redirect:/dashboard";
+//    }
 }
