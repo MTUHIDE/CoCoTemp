5ff4e6a1cd39e39911bbc11125859f582759aae2
doughepi <doughertypiper@gmail.com>
2017-01-25 23:53:13 -0500

    Forms are mostly laid out.

--- src/main/java/space/hideaway/controllers/SettingsController.java
+++ src/main/java/space/hideaway/controllers/SettingsController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import space.hideaway.model.User;
+import space.hideaway.services.DeviceService;
 import space.hideaway.services.UserManagementImpl;
+
+import java.util.UUID;
 
 /**
  * Created by dough on 1/25/2017.
@@ -15,6 +19,8 @@ public class SettingsController
 {
     @Autowired
     UserManagementImpl userManagement;
+    @Autowired
+    DeviceService deviceService;
 
     @RequestMapping(value = "/settings")
     public String showSettings(Model model)
@@ -24,11 +30,12 @@ public class SettingsController
         return "/settings/general";
     }
 
-    @RequestMapping(value = "/settings/profile")
-    public String defaultProfile(Model model)
+    @RequestMapping(value = "/settings/device", params = {"deviceID"})
+    public String loadDevice(Model model, @RequestParam("deviceID") UUID deviceID)
     {
         User currentLoggedInUser = userManagement.getCurrentLoggedInUser();
         model.addAttribute("devices", currentLoggedInUser.getDeviceSet());
-        return "/settings/profile";
+        model.addAttribute("currentDevice", deviceService.findByKey(deviceID.toString()));
+        return "/settings/device";
     }
 }
