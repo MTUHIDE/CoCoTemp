8bec8c6691016e07ace3b320d2202322c6dff0bb
Kyle Bray <kpbray@mtu.edu>
2018-01-24 02:36:14 -0500

    Added filter side menu to microclimate page. Made marker color correspond with graph color. Removed search feature from side menu

--- src/main/resources/static/js/microclimate.js
+++ src/main/resources/static/js/microclimate.js
@@ -1,97 +1,21 @@
 $(document).ready(function() {
 
-    // Search functionality
-    $('#searchButton').click(function() {
-        var locQuery;
-        var latQuery = $('#latitude-query').val();
-        var lonQuery = $('#longitude-query').val();
-        if (latQuery === null || latQuery === '' || lonQuery === null || lonQuery === '') {
-            locQuery = '';
-        } else {
-            locQuery = latQuery + "," + lonQuery;
-        }
-        populateSearchResults($('#search-query').val(), locQuery, $('#range-selector').val());
-    });
-
-    // Create map
+    // Create map of sites
     var myMap = createMap();
     $('#basemaps').on('change', function() {
         changeBasemap(myMap, this.value);
     });
     populateMapWithSites(myMap);
 
-    //Add autocomplete to location search
-    var $location = document.getElementById('location-query');
-    var autocomplete = new google.maps.places.Autocomplete($location);
-    autocomplete.addListener("place_changed", function () {
-        var location = autocomplete.getPlace().geometry.location;
-        $("#latitude-query").val(location.lat());
-        $("#longitude-query").val(location.lng());
-    });
-
+    // Create site temperature line chart
     createChart();
 });
 
-function myFunction () {
-    $("#sidebar").toggleClass("collapsed");
-    $("#content").toggleClass("col-md-12 col-md-9");
+function expandFilterSideMenu () {
+    $("#filter-menu").toggleClass("collapsed");
+    $("#sidemenu-popup").toggleClass("glyphicon-menu-right glyphicon-menu-left").toggleClass("side-menu-flat side-menu-expand");
+    $("#content").toggleClass("content-flat content-expand");
 };
-
-function populateSearchResults(query, location, range) {
-    var urlString = "/cocotemp/search.json?type=site"
-    if(query !== null && query !== '') {
-        urlString = urlString + "&query=" + query;
-    }
-    if(location !== null && location !== '') {
-        urlString = urlString + "&location=" + location;
-    }
-    if(range !== null && range !== '') {
-        urlString = urlString + "&range=" + range;
-    }
-    var table = $("#search-results");
-    table.find("tr:gt(0)").remove();
-    $.ajax({
-        method: 'post',
-        url: urlString,
-        success: function (data) {
-            if (data.length === 0) {
-                return;
-            }
-            for (var i = 0; i < data.length; i++) {
-                var row = $("<tr>")
-                    .append($("<td>").text(data[i].siteName))
-                    .append($("<td>").text(data[i].siteDescription));
-                table.append(row);
-            }
-        }
-    });
-
-
-
-    // function populateChart() {
-    //
-    //     $.ajax({
-    //         method: 'get',
-    //         url: "/cocotemp/site/" + siteID + "/temperature.json",
-    //         success: function (data) {
-    //             data.forEach(function (datum) {
-    //                 dates.push(new Date(datum['dateTime']));
-    //                 temperature.push(datum['temperature']);
-    //             });
-    //         }
-    //     });
-    //
-    //     var collectedTemps = {
-    //         x: dates,
-    //         y: temperature,
-    //         name: 'site\'s temperature',
-    //         type: 'scatter'
-    //     };
-    //     var data = [collectedTemps];
-    //
-    //
-    // }
-}
 
 var data = [];
 var layout;
@@ -132,38 +56,38 @@ function createChart() {
         margin: {r: 100}
     };
 
-    thresholds.forEach(function(threshold) {
-        var lines = {
-            type: 'line',
-            xref: 'paper',
-            yref: 'y',
-            x0: 0,
-            y0: threshold.thresholdValue,
-            x1: 1,
-            y1: threshold.thresholdValue,
-            line: {
-                color: 'rgb(0, 0, 255)',
-                width: 1
-            }
-        };
-        layout.shapes.push(lines);
-
-        var annotations = {
-            xref: 'paper',
-            x: 1,
-            y: threshold.thresholdValue,
-            xanchor: 'left',
-            yanchor: 'middle',
-            text: threshold.thresholdName,
-            showarrow: false,
-            font: {
-                family: 'Segoe UI',
-                size: 14,
-                color: '#7f7f7f'
-            }
-        };
-        layout.annotations.push(annotations);
-    });
+    // thresholds.forEach(function(threshold) {
+    //     var lines = {
+    //         type: 'line',
+    //         xref: 'paper',
+    //         yref: 'y',
+    //         x0: 0,
+    //         y0: threshold.thresholdValue,
+    //         x1: 1,
+    //         y1: threshold.thresholdValue,
+    //         line: {
+    //             color: 'rgb(0, 0, 255)',
+    //             width: 1
+    //         }
+    //     };
+    //     layout.shapes.push(lines);
+    //
+    //     var annotations = {
+    //         xref: 'paper',
+    //         x: 1,
+    //         y: threshold.thresholdValue,
+    //         xanchor: 'left',
+    //         yanchor: 'middle',
+    //         text: threshold.thresholdName,
+    //         showarrow: false,
+    //         font: {
+    //             family: 'Segoe UI',
+    //             size: 14,
+    //             color: '#7f7f7f'
+    //         }
+    //     };
+    //     layout.annotations.push(annotations);
+    // });
 
     var indexTemp = [0, 31, 35, 56];
     var indexColors = ['rgb(0, 0, 255)', 'rgb(255, 255, 51)', 'rgb(255, 215, 0)', 'rgb(255, 0, 0)'];
@@ -246,13 +170,16 @@ function populateMapWithSites(myMap) {
                 var point = L.latLng([data[i].siteLatitude, data[i].siteLongitude]);
                 var myMarker = new siteMarker(point,{options: {siteID: data[i].id, siteName: data[i].siteName, onChart: false} });
                 myMarker.addTo(myMap).on('click',markerClick);
-                //myMarker.bindPopup('<a href="site/' + data[i].id + '">' + data[i].siteName + '</a>');
                 siteMarkers.push(myMarker);
             }
         }
     });
 }
 
+/*
+ * Action upon click of map marker. Adds and removes data/line to line chart.
+ */
+var colors = ['black', 'green', 'red', 'violet', 'yellow'];
 function markerClick(e) {
     if (e.target.options.options.onChart === true) {
         e.target.setIcon(new L.Icon.Default() );
@@ -276,14 +203,17 @@ function markerClick(e) {
                 x: dates,
                 y: temperature,
                 name: e.target.options.options.siteName,
-                type: 'scatter'
+                type: 'scatter',
+                marker: {
+                    color: colors[data.length]
+                }
             };
             data.push(collectedTemps);
             var chart = document.getElementById('temperature-chart');
             Plotly.newPlot(chart, data, layout);
 
             var greenIcon = new L.Icon({
-                iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+                iconUrl: "/cocotemp/images/marker-icon-2x-" + colors[data.length - 1] + ".png",
                 shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
                 iconSize: [25, 41],
                 iconAnchor: [12, 41],
