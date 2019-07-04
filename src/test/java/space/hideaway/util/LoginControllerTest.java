fcab318c3e3d9b76dbe0fac35b4a2200eb948150
collin <ckpalmer@mtu.edu>
2019-07-03 23:54:24 -0400

    Collin Palmer 07/03/19 Added tests for the three controllers labeled as login controllers.

---
+++ src/test/java/space/hideaway/util/LoginControllerTest.java
@@ -0,0 +1,26 @@
+package space.hideaway.util;
+import org.junit.Assert;
+import org.springframework.ui.Model;
+import space.hideaway.controllers.login.LoginController;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+
+public class LoginControllerTest {
+
+    @Test
+        public void TestShowLogin(){
+        LoginController loginController = new LoginController();
+        Assert.assertEquals("login/login",loginController.showLogin());
+    }
+    @Test
+        public void TestShowLoginError(){
+        Model mockModel = mock(Model.class);
+        LoginController loginController = new LoginController();
+        Assert.assertEquals("login/login",loginController.showLoginError(mockModel));
+        verify(mockModel).addAttribute("loginError",true);
+
+    }
+
+}
---
+++ src/test/java/space/hideaway/util/ResetPasswordControllerTest.java
@@ -0,0 +1,42 @@
+package space.hideaway.util;
+
+import org.junit.*;
+import org.mockito.*;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import space.hideaway.controllers.login.ResetPasswordController;
+import space.hideaway.model.User;
+import space.hideaway.model.security.GenericResponse;
+import space.hideaway.services.user.UserToolsService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.ws.RequestWrapper;
+
+import java.util.Locale;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ResetPasswordControllerTest {
+
+    @Test
+        public void TestShowChangePasswordPage(){
+
+        Locale expectedLocale = new Locale("English","USA");
+
+        Model MockModel = mock(Model.class);
+        long expectedId = (long) 45;
+        String expectedToken  = "Token";
+
+        ResetPasswordController resetPasswordController = new ResetPasswordController();
+
+        String expectedWebPageRedirect = "redirect:/updatePassword.html?lang="+ expectedLocale.getLanguage();
+
+        Assert.assertEquals(expectedWebPageRedirect,resetPasswordController.showChangePasswordPage(expectedLocale,MockModel,expectedId,expectedToken));
+
+    }
+
+
+}
