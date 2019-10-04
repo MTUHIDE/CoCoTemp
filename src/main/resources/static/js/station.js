b334a0b89e5e615f6e49db5f7e50faa6f96e135d
cpalme15 <cpalme15@emich.edu>
2019-10-04 18:05:38 -0400

    Collin Palmer 10/4/19 Change globe questionnaire question 11 and 12 to reflect what ben wanted

--- src/main/resources/static/js/station.js
+++ src/main/resources/static/js/station.js
@@ -10,18 +10,18 @@ $(function () {
                     document.getElementById("globeMeta").innerHTML="<p>Globe Survey Not Filled Out</p>";
                 }
                 else{
-                    document.getElementById("Environment").textContent="Environment: "+z.environment;
-                    document.getElementById("Purpose").textContent="Purpose: "+z.purpose;
-                    document.getElementById("HeightAboveGround").textContent="Height Above Ground: "+z.heightAboveGround+" m";
-                    document.getElementById("HeightAboveFloor").textContent="Height Above Floor: "+z.heightAboveFloor+" m";
+                    document.getElementById("Environment").textContent="Environment the site is in: "+z.environment;
+                    document.getElementById("Purpose").textContent="Purpose of the area where the site is located: "+z.purpose;
+                    document.getElementById("HeightAboveGround").textContent="Height the sensor is above the ground: "+z.heightAboveGround+" m";
+                    document.getElementById("HeightAboveFloor").textContent="Height the sensor is above the floor: "+z.heightAboveFloor+" m";
                     document.getElementById("Enclosure").textContent="Enclosure Percentage: "+z.enclosurePercentage+"%";
                     document.getElementById("AirObst").textContent="Nearest Airflow Obstacle: "+z.nearestAirflowObstacle+" m";
                     document.getElementById("ObstDeg").textContent="Nearest Obstacle Degrees: "+z.nearestObstacleDegrees+String.fromCharCode(176);
                     document.getElementById("ObstType").textContent="Obstacle Type: "+z.obstacleType;
                     document.getElementById("SenArea").textContent="Area Around Sensor: "+z.areaAroundSensor+" m";
                     document.getElementById("RipArea").textContent="Located in a depression or Riparian Area: "+z.riparianArea;
-                    document.getElementById("maxNight").textContent="Hours shaded on longest day: "+z.maxNightTime+ " hrs";
-                    document.getElementById("minNight").textContent="Hours shaded on shortest day: "+z.minNightTime+" hrs";
+                    document.getElementById("maxNight").textContent="Hours unshaded and exposed to the sun on the longest day: "+z.maxNightTime+ " hrs";
+                    document.getElementById("minNight").textContent="Hours unshaded and exposed to the sun on the shortest day: "+z.minNightTime+" hrs";
                     document.getElementById("Canopy").textContent="Canopy Type: "+z.canopyType;
                     document.getElementById("skyView").textContent="SkyView Factor: "+z.skyViewFactor+"%";
                     document.getElementById("Slope").textContent="Slope of site location: "+z.slope+"%";
