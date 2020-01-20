484c91afd047b94f7a2bbd207835db267e3437d8
collin <ckpalmer@mtu.edu>
2020-01-20 13:39:33 -0500

    Collin Palmer 01/20/2019 One import change from last commit didn't get comitted somehow

--- src/main/java/space/hideaway/CoCoTempApplication.java
+++ src/main/java/space/hideaway/CoCoTempApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
