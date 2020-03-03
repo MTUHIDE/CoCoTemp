ac2136af57d4f8cc657497d694634d05c8195464
collin <ckpalmer@mtu.edu>
2020-03-03 11:00:21 -0500

    Collin Palmer 03/03/20 Made changes to acknowledgments page

--- src/main/resources/static/js/acknowledgement.js
+++ src/main/resources/static/js/acknowledgement.js
@@ -3,39 +3,46 @@ $(function () {
     // Author: Trevor Murphy
     text = '';
     var people = [
-        ['Dr. Ben Ruddell', 'Project Director'],
-        ['Dr. Robert Pastel', 'Development Lead'],
-        ['Paul Bahle', 'Former Human Factors Specialist'],
-        ['Tom Berg','Web Developer (2019-present)'],
-        ['Kyle Bray', 'Former Web/Sensor Developer (2016-2018)'],
-        ['Payton Dunning', 'Former Web Developer (2017-2018)'],
-        ['Piper Dougherty', 'Former Web Developer (2016-2017)'],
-        ['Paul Esch-Laurent', 'Former Sensor/Web Developer (2016-2018)'],
-        ['Connor Gessner', 'Former Circuits Developer'],
-        ['Justin Havely', 'Former Web Developer (2017-2018)'],
-        ['Alex Israels','Former Sensor Developer (2018-2019)'],
-        ['Alejandro Jeronimo Ayala Perez','Former Web Developer (2018-2019)'],
-        ['Zachary Johnson','Former Web Developer (2018-2019)'],
-        ['Will Kirkconnell','Former Web Developer (2018-2019)'],
-        ['Nick Lanter', 'Former Sensor Developer (2016-2017)'],
-        ['Gary Lord', 'Former Sensor Developer'],
-        ['Caleb Melnychenko','Web Developer (2018-present)'],
-        ['Trevor Murphy', 'Former Web Developer (2017-2018)'],
-        ['Abby Myers', 'Web Developer (2020-present)'],
-        ['Collin Palmer','Web/Sensor Developer (2018-present)'],
-        ['Cody Reightley', 'Web Developer (2019-present)'],
-        ['Mason Sayles','Web Developer (2019-present)'],
-       ['Jake Schmitz', 'Former Web Developer'],
-        ['Benjamin Slade','Former Web Developer (2018-2019)'],
-        ['Caden Sumner', 'Former Web Developer (2016-2017)'],
-        ['Reo Tang','Former Web Developer (2018-2019)'],
-        ['Brendan Zondlak','Former Sensor Developer (2018-2019)']
+        ['Dr. Ben Ruddell', 'Project Director','Northern Arizona University','NAU'],
+        ['Dr. Robert Pastel', 'Development Lead','Michigan Technological University','MTU'],
+        ['Paul Bahle', 'Former Human Factors Specialist','Michigan Technological University','MTU'],
+        ['Tom Berg','Web Developer (2019-present)','Michigan Technological University','MTU'],
+        ['Kyle Bray', 'Former Web/Sensor Developer (2016-2018)','Michigan Technological University','MTU'],
+        ['Payton Dunning', 'Former Web Developer (2017-2018)','Michigan Technological University','MTU'],
+        ['Piper Dougherty', 'Former Web Developer (2016-2017)','Michigan Technological University','MTU'],
+        ['Paul Esch-Laurent', 'Former Sensor/Web Developer (2016-2018)','Michigan Technological University','MTU'],
+        ['Connor Gessner', 'Former Circuits Developer','Michigan Technological University','MTU'],
+        ['Justin Havely', 'Former Web Developer (2017-2018)','Northern Arizona University','NAU'],
+        ['Alex Israels','Former Sensor Developer (2018-2019)','Michigan Technological University','MTU'],
+        ['Alejandro Jeronimo Ayala Perez','Former Web Developer (2018-2019)','Michigan Technological University','MTU'],
+        ['Zachary Johnson','Former Web Developer (2018-2019)','Michigan Technological University','MTU'],
+        ['Will Kirkconnell','Former Web Developer (2018-2019)','Michigan Technological University','MTU'],
+        ['Nick Lanter', 'Former Sensor Developer (2016-2017)','Michigan Technological University','MTU'],
+        ['Gary Lord', 'Former Sensor Developer','Michigan Technological University','MTU'],
+        ['Caleb Melnychenko','Web Developer (2018-present)','Michigan Technological University','MTU'],
+        ['Trevor Murphy', 'Former Web Developer (2017-2018)','Michigan Technological University','MTU'],
+        ['Abby Myers', 'Web Developer (2020-present)','Michigan Technological University','MTU'],
+        ['Collin Palmer','Web/Sensor Developer (2018-present)','Michigan Technological University','MTU'],
+        ['Cody Reightley', 'Web Developer (2019-present)','Michigan Technological University','MTU'],
+        ['Mason Sayles','Web Developer (2019-present)','Michigan Technological University','MTU'],
+       ['Jake Schmitz', 'Former Web Developer','Northern Arizona University','NAU'],
+        ['Benjamin Slade','Former Web Developer (2018-2019)','Michigan Technological University','MTU'],
+        ['Caden Sumner', 'Former Web Developer (2016-2017)','Michigan Technological University','MTU'],
+        ['Reo Tang','Former Web Developer (2018-2019)','Michigan Technological University','MTU'],
+        ['Brendan Zondlak','Former Sensor Developer (2018-2019)','Michigan Technological University','MTU']
     ];
     for ( i = 0; i < people.length; i++ ) {
         if (i % 3 == 0) {
             text += '</div><div class="row">';
         }
-        text += '<div class="col-lg-4"> <div class="panel-panel-default"> <div class="panel-body"> <div class="about"> <h2>' + people[i][0] + '<br/><small> ' + people[i][1] + '</small></h2> <hr /> </div> </div> </div> </div>';
+        if(people[i][3]=='NAU')
+        {
+            text += '<div class="col-lg-4"> <div class="panel-panel-default"> <div class="panel-body"> <div class="about"> <h2>' + people[i][0] + '<br/><small> ' + people[i][1] + '</small><br/> <small> ' + 'Institution: '+ people[i][2] + '</small>  </h2> <img style = "height: 100px; width:150px" src="/cocotemp/images/NAU.png"> <hr /> </div> </div> </div> </div>';
+        }
+        else if(people[i][3]=='MTU')
+        {
+            text += '<div class="col-lg-4"> <div class="panel-panel-default"> <div class="panel-body"> <div class="about"> <h2>' + people[i][0] + '<br/><small> ' + people[i][1] + '</small><br/> <small> ' + 'Institution: ' + people[i][2] + '</small>  </h2> <img style = "height: 100px; width:200px" src="/cocotemp/images/MTU.png"> <hr /> </div> </div> </div> </div>';
+        }
     }
     document.getElementById('acknowledgeStart').innerHTML = text;
 });
